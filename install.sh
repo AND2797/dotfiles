56f2a542f8501312480593f800b4694c32b975ff
AND2797 <aditya.das2797@gmail.com>
2021-06-07 00:02:50 -0400

    check if nvim dir exists

--- install.sh
+++ install.sh
@@ -8,7 +8,11 @@ mkdir -p $olddir
 cd $dir
 
 for file in $files; do
-	mv ~/.config/$file ~/dotfiles_old/
+    if [ -d "~/.config/$file"]
+    then 
+	    mv ~/.config/$file ~/dotfiles_old/
+    fi
+
 	echo "Creating symlink to $file in home dir."
 	ln -s $dir/$file ~/.config/$file
 done
